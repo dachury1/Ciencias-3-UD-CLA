03f6a684e9f44009e542442d7f7c8258acc83c2f
Leykat <lecvargasm@correo.udistrital.edu.co>
2018-08-20 16:44:47 -0500

    Parqueadero

--- EjemploParqueo.py
+++ EjemploParqueo.py
@@ -8,7 +8,8 @@
 # Copyright:   (c) LEIDY K 2018
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-
+import random
+import string
 class Cola (object):
 
     def __init__(self):
@@ -55,20 +56,21 @@ class Cola (object):
             for i in self.items:
                 if i.getPropietario() == x:
                     return "Registrado: "+i.descripcion()
-        return "No se encontr0 moto con dicho propietario";
+        return "No se encontro moto con dicho propietario";
 
 
 
 class Moto:
 
-    def __init__(self, placa, ficha, propietario):
 
-        self.placa = placa
-        self.ficha = ficha
-        self.prop = propietario
+    def __init__(self, propietario,placa,codigo, ficha):
+            self.placa = placa
+            self.ficha = ficha
+            self.propietario = propietario
+            self.cod= codigo
 
     def descripcion (self):
-        return self.placa +" "+ self.ficha +" "+ self.prop
+        return self.placa +" "+ self.ficha +" "+self.cod+" "+ self.propietario
 
     def getPlaca (self):
         return self.placa+""
@@ -77,47 +79,45 @@ class Moto:
         return self.ficha+""
 
     def getPropietario (self):
-        return self.prop+""
+        return self.propietario+""
 
-    def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-     return ''.join(random.choice(chars) for _ in range(size))
-
+    def getCodigo (self):
+        return self.cod+""
 
 def main ():
-    usuario = Cola()
-while True:
+   while True:
     usuario = Cola()
     print("1. Registrar")
     print("2. Consulta por Placa")
     print("3. Consulta por Nombre Propietario ")
     print("4. Consulta por Ficha ")
     print("5. Registros ")
-    conPlaca= input()
-    conNom= input()
-    conFicha= input()
     opcion = int(input())
     if opcion == 1:
         placa=raw_input("Placa\n")
         nom=raw_input("Nombre Usuario\n")
         ident=int(input("Carnet\n"))
-        ficha=id_generator(6)
+        ficha= random.randint(0,100)
         motos=Moto(nom,placa,ident,ficha)
         usuario.encolar(motos)
     elif opcion==2:
-      print(usuario.buscarPlaca(conPlaca))
+      placa=raw_input("Placa\n")
+      print(usuario.buscarPlaca(placa))
       usuario.desencolar()
     elif opcion==3:
-     print(usuario.buscarPropietario(conNom))
-     usuario.desencolar()
-    elif opcion==4:
-        print(usuario.buscarFicha(conFicha))
+        nom=raw_input("Nombre Usuario\n")
+        print(usuario.buscarPropietario(nom))
         usuario.desencolar()
+    elif opcion==4:
+        ficha=raw_input("Ficha\n")
+        print(usuario.buscarFicha(ficha))
+        usuario.desencolar()
+    elif opcion==5:
+        print(usuario.mirarElementos())
     else:
-print(usuario.mirarElementos())
+        print("Opcion no valida")
         break
 
 
 
 main()
-
-
